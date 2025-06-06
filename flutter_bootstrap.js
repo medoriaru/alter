ca146a5dae0f74a41a69d0284a12d5b15c19178f
Ivan MacOS <fortran.new@gmail.com>
2025-06-06 15:36:36 +0700

    Добавление, изменение и удаление пользователей

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2417139834"
+    serviceWorkerVersion: "3125803031"
   }
 });
