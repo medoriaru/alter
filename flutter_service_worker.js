10367c666516daf07eab58481e88404db057ca03
Ivan MacOS <fortran.new@gmail.com>
2025-06-10 20:45:03 +0700

    Ошибка подключения устройства по-умолчанию (выбранного на этапе подготовке)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,13 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "0e510663c3104b25f5d87237f1099e5d",
+const RESOURCES = {"flutter_bootstrap.js": "b9320ef9ad1b64bceea5a49426086063",
 "version.json": "34f53a3c11c9cb1d61ae18b6faf64622",
 "favicon.ico": "e960c12aa5a5413f62a17e9b79addb3a",
 "index.html": "9d199e2103e2492e334e0d26f84df436",
 "/": "9d199e2103e2492e334e0d26f84df436",
 "firebase-messaging-sw.js": "d18d9cc6172169056830dcf7b817e9b0",
-"main.dart.js": "8be713ee586ac730249599945e3c8b4d",
+"main.dart.js": "7293efd74fc5cb917ff6e95882117ffc",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "icons/Icon-192.png": "c64bfe553f072ba7ee4e6a79bffb1a41",
 "icons/Icon-maskable-192.png": "cec42d926f7580e15452d2f97662e07b",
