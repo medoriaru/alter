4d910301492cf71386b2c15a1f8c55cfd4c9ae7b
Ivan MacOS <fortran.new@gmail.com>
2025-06-13 01:19:32 +0700

    Система оплаты

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,13 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "de404e81d4b061bb75e34e581f895309",
+const RESOURCES = {"flutter_bootstrap.js": "ca0b48577c09f901e8a559162111ecfb",
 "version.json": "f859f7f14291fe3f639709c20f1461ee",
 "favicon.ico": "e960c12aa5a5413f62a17e9b79addb3a",
 "index.html": "9d199e2103e2492e334e0d26f84df436",
 "/": "9d199e2103e2492e334e0d26f84df436",
 "firebase-messaging-sw.js": "d18d9cc6172169056830dcf7b817e9b0",
-"main.dart.js": "778ee4ece2316a9c0e8a89e3d71bd4eb",
+"main.dart.js": "b92d28ad299d707d5dfde2d74f4738ef",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "icons/Icon-192.png": "c64bfe553f072ba7ee4e6a79bffb1a41",
 "icons/Icon-maskable-192.png": "cec42d926f7580e15452d2f97662e07b",
